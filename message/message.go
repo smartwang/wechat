3a4d8e890aef76a4c15fc12f1a9abec85ed1bcf3
wangying <wangying@dragonest.com>
2019-01-02 16:43:32 +0800

    bug fix

--- message/message.go
+++ message/message.go
@@ -7,7 +7,7 @@ type Message interface {
 	Decrypt(encryptText, key string) (plantText string, err error)
 	VerifyURL(echoStr string) (string, error)
 	Package(msg string) (string, error)
-	Parse(data string) (interface{}, error)	// 解析请求body
+	Parse(data []byte) (interface{}, error)	// 解析请求body
 	// TODO: 实现其他如获取用户名、消息类型等信息的方法
 }
 
